4ef93bab7265f8a7f9ef0e521d4bb3a9991b4550
Nikita60012 <96587581+Nikita60012@users.noreply.github.com>
2022-03-16 01:00:54 +0300

    Add files via upload

---
+++ Лабораторная работа №4/src/MyProgram.java
@@ -0,0 +1,30 @@
+
+public class MyProgram {
+    public static void main(String[] args){
+    int length = (int) (Math.random() *((10-2)+1))+2;
+    System.out.println("Размерность массива равна " + length);
+
+    int[] myArray = new int[length];
+    System.out.println("Состав массива: ");
+
+    for (int i = 0; i < length; i++) {
+        myArray[i] = (int) (Math.random() * ((99)+1))+1;
+        System.out.print(myArray[i] + " ");
+    }
+
+    System.out.println();
+
+    int min = 102;
+    double sum = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (myArray[i] < min)
+            min = myArray[i];
+        sum += myArray[i];
+    }
+
+    sum /= length;
+    sum += min;
+    System.out.println("Сумма минимального элемента и среднего арифметического равна " + sum);
+    }
+}
